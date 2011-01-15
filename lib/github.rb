035e396fa72a40be74dcaa318039c6fab13b9fea
Curtis Schofield <github@robotarmyma.de>
2011-01-15 13:44:43 -0800

    e

--- lib/github.rb
+++ lib/github.rb
@@ -1,16 +1,35 @@
 require 'socket'
 def github
   return Object.new.instance_eval do
+    attr_accessor :raw_in, :headers, :body
+    def debug(client)
+      puts self.raw_in
+      puts self.headers
+      puts self.body
+    end
     def server
       @server ||= TCPServer.open(9898)
+    end
+    def in(client)
+      self.raw_in = client.readlines
+      self.headers = raw_in[0..4]
+      self.body = raw_in.last
+    end
+    def out(client)
+        client.puts(Time.now.ctime)  # Send the time to the client
+        client.puts "Closing the connection. Bye!"
+    end
+    def accept
+        client = server.accept       # Wait for a client to connect
+        in(client)
+        debug(client)
+        out(client)
+        client.close                 # Disconnect from the client
     end
     def listen
       puts "waiting.."
       loop {                         # Servers run forever
-        client = server.accept       # Wait for a client to connect
-        client.puts(Time.now.ctime)  # Send the time to the client
-        client.puts "Closing the connection. Bye!"
-        client.close                 # Disconnect from the client
+        accept
       }
       self
     end
